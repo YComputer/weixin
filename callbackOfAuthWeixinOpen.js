10b5082157b16f4338f2915711f34a9e3e769956
YComputer <thea@163.com>
2016-05-31 10:56:44 +0800

    refactor

--- callbackOfAuthWeixinOpen.js
+++ callbackOfAuthWeixinOpen.js
@@ -22,6 +22,10 @@ var tpl = heredoc(function() {/*
           <div id="baseInfo">'<%= baseInfo %>'</div>
           <h1>公众号关注者信息<h1>
           <div id="followInfo">'<%= followInfo %>'</div>
+          <h1>图文群发每日数据<h1>
+          <div id="articleSummary">'<%= articleSummary %>'</div>
+
+
 
           <script src="http://zeptojs.com/zepto-docs.min.js"></script>
           <script src="http://res.wx.qq.com/open/js/jweixin-1.1.0.js"></script>
@@ -45,13 +49,13 @@ module.exports = function(config) {
       var componentAccessToken = yield utils.readFileAsync(component_access_token_file, 'utf-8')
       console.log('componentAccessToken is: ',componentAccessToken)
       //----- 使用授权码换取公众号的接口调用凭据和授权信息 start
-      var form = {
+      var formAuthInfo = {
               component_appid: config.weixinOpenGongzhonghao.appID,
               authorization_code: this.query.auth_code
             }
-      var url = 'https://api.weixin.qq.com/cgi-bin/component/api_query_auth?component_access_token=' + componentAccessToken
+      var urlAuthInfo = 'https://api.weixin.qq.com/cgi-bin/component/api_query_auth?component_access_token=' + componentAccessToken
       var authInfo = yield new Promise(function(resolve, reject) {
-                  request({method: 'POST',url: url, body: form, json: true}).then(function(response) {
+                  request({method: 'POST',url: urlAuthInfo, body: formAuthInfo, json: true}).then(function(response) {
                       var body = response.body
                       resolve(body)
                   })
@@ -59,32 +63,46 @@ module.exports = function(config) {
       console.log('使用授权码换取公众号的接口调用凭据和授权信息\n', authInfo)
       //----- 使用授权码换取公众号的接口调用凭据和授权信息 end
       //----- 获取公众号基本信息 start
-      var form2 = {
+      // 存储已经授权过的公众号
+      var formBaseInfo = {
               component_appid: config.weixinOpenGongzhonghao.appID,
               authorizer_appid: authInfo.authorization_info.authorizer_appid
             }
-      var url2 = 'https://api.weixin.qq.com/cgi-bin/component/api_get_authorizer_info?component_access_token=' + componentAccessToken
-      var body2 = yield new Promise(function(resolve, reject) {
-                  request({method: 'POST',url: url2, body: form2, json: true}).then(function(response) {
+      var urlBaseInfo = 'https://api.weixin.qq.com/cgi-bin/component/api_get_authorizer_info?component_access_token=' + componentAccessToken
+      var baseInfo = yield new Promise(function(resolve, reject) {
+                  request({method: 'POST',url: urlBaseInfo, body: formBaseInfo, json: true}).then(function(response) {
                       var body = response.body
                       resolve(body)
                   })
               })
 
-      params.baseInfo=JSON.stringify(body2)
+      params.baseInfo=JSON.stringify(baseInfo)
       // 获取公众号基本信息 end
       //----- 获取公众号关注者信息 start
-      var url3 = 'https://api.weixin.qq.com/cgi-bin/user/get?access_token='+authInfo.authorization_info.authorizer_access_token
+      var urlFllowInfo = 'https://api.weixin.qq.com/cgi-bin/user/get?access_token='+authInfo.authorization_info.authorizer_access_token
 
-      var body3 = yield new Promise(function(resolve, reject) {
-                  request({method: 'GET',url: url3, json: true}).then(function(response) {
+      var fllowInfo = yield new Promise(function(resolve, reject) {
+                  request({method: 'GET',url: urlFllowInfo, json: true}).then(function(response) {
                       var body = response.body
                       resolve(body)
                   })
               })
-      console.log(url3)
-      params.followInfo=JSON.stringify(body3)
+      params.followInfo=JSON.stringify(fllowInfo)
       // 获取公众号关注者信息 end
+      //----- 获取图文群发每日数据 start
+      var getArticleSummary = 'https://api.weixin.qq.com/datacube/getarticlesummary?access_token='+authInfo.authorization_info.authorizer_access_token
+      var datBetween = {
+              begin_date: '2015-12-08',
+              end_date: '2015-12-08'
+            }
+      var articleSummary = yield new Promise(function(resolve, reject) {
+                  request({method: 'POST',url: getArticleSummary, body: datBetween, json: true}).then(function(response) {
+                      var body = response.body
+                      resolve(body)
+                  })
+              })
+      params.articleSummary=JSON.stringify(articleSummary)
+      // 获取图文群发每日数据 end
 
 
       this.body = ejs.render(tpl, params)
