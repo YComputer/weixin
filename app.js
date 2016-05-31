0b64a1db0e1489b277f2033524892a851e5feffd
YComputer <thea@163.com>
2016-05-31 18:24:23 +0800

    refactor

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var authWeixinOpen = require('./authWeixinOpen')
 var callbackOfAuthWeixinOpen = require('./callbackOfAuthWeixinOpen')
 var rootRouter = require('./rootRouter')
 var gongZhongHaoMiddleware = require('./gongZhongHaoMiddleware')
+var gongZhongHaoHandler = require('./gongZhongHaoHandler')
 
 var port = 80
 
@@ -14,7 +15,7 @@ var app = new koa()
 app.use(route.get('/authWeixinOpen', authWeixinOpen(config)))
 app.use(route.get('/callbackOfAuthWeixinOpen', callbackOfAuthWeixinOpen(config)))
 app.use(gongZhongHaoMiddleware(config.weixinGongzhonghao))
-app.use(rootRouter(config.weixinOpenGongzhonghao))
+app.use(rootRouter(config.weixinOpenGongzhonghao, gongZhongHaoHandler.reply))
 
 app.listen(port)
 console.log('listening port is: '+ port)
