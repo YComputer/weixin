b158202a32cbf644096d2313b6ba012c53f747c5
YComputer <thea@163.com>
2016-05-31 14:44:25 +0800

    add koa-route

--- app.js
+++ app.js
@@ -2,17 +2,19 @@
 
 var koa = require('koa')
 var route = require('koa-route')
-var Promise = require('bluebird')
-var sha1 = require('sha1')
-var getRawBody = require('raw-body')
-var utils = require('./utils')
-var WxCrypto = require('./wxCrypto')
-var path = require('path')
-var ejs = require('ejs')
-var heredoc = require('heredoc')
-var verify_ticket_file = path.join(__dirname, './verify_ticket.txt')
+// var Promise = require('bluebird')
+// var sha1 = require('sha1')
+// var getRawBody = require('raw-body')
+// var utils = require('./utils')
+// var WxCrypto = require('./wxCrypto')
+// var path = require('path')
+// var ejs = require('ejs')
+// var heredoc = require('heredoc')
+// var verify_ticket_file = path.join(__dirname, './verify_ticket.txt')
 var authWeixinOpen = require('./authWeixinOpen')
 var callbackOfAuthWeixinOpen = require('./callbackOfAuthWeixinOpen')
+var rootRouter = require('./rootRouter')
+
 
 var port = 80
 var config = {
@@ -27,60 +29,7 @@ var config = {
 var app = new koa()
 app.use(route.get('/authWeixinOpen', authWeixinOpen(config)))
 app.use(route.get('/callbackOfAuthWeixinOpen', callbackOfAuthWeixinOpen(config)))
-
-// app.use(authWeixinOpen(config))
-// app.use(callbackOfAuthWeixinOpen(config))
-
-app.use(function* (next){
-  console.log(this.query)
-  var token = config.weixinOpenGongzhonghao.token
-  var signature = this.query.signature
-  var nonce = this.query.nonce
-  var timestamp = this.query.timestamp
-  var echostr = this.query.echostr
-
-  var str = [token, timestamp, nonce].sort().join('')
-  var sha1Str = sha1(str)
-
-  if(sha1Str === signature){
-    if(this.method === 'GET'){
-      console.log('request from weixin server↓↓↓↓\n' +
-          ' method is %s \n path is %s \n query is %s \n' +
-          'request from weixin server↑↑↑↑',
-          this.method, this.url, JSON.stringify(this.query))
-    }else if(this.method === 'POST'){
-      var data = yield getRawBody(this.req, { length: this.length, limit: '1mb', encoding: this.charset })
-
-      console.log('request from weixin server↓↓↓↓\n' +
-          ' method is %s \n path is %s \n data is %s \n' +
-          'request from weixin server↑↑↑↑',
-          this.method, this.url, data.toString())
-
-      var content = yield utils.parseXMLAsync(data)
-      console.log('raw data to json object\n', content)
-      var message = utils.formatMessage(content.xml)
-      console.log('json object to plain json object\n', message)
-
-      var xmlVerifyTicket = new WxCrypto(config.weixinOpenGongzhonghao.token, config.weixinOpenGongzhonghao.appID, config.weixinOpenGongzhonghao.key).decrypt(message.Encrypt)
-      var rawVerifyTicket = yield utils.parseXMLAsync(xmlVerifyTicket)
-      var verifyTicket = utils.formatMessage(rawVerifyTicket.xml)
-      console.log('componentVerifyTicket is: ', JSON.stringify(verifyTicket))
-
-      // save verifyTicket
-      utils.writeFileAsync(verify_ticket_file, verifyTicket.ComponentVerifyTicket)
-
-    }
-    this.body = 'What is happen?????'
-  }else{
-    console.log('request from other↓↓↓↓\n' +
-        ' method is %s \n path is %s \n query is %s \n' +
-        'request from other↑↑↑↑',
-        this.method, this.url, JSON.stringify(this.query))
-
-    this.body = '微信server以外的请求'
-  }
-
-})
+app.use(rootRouter(config))
 
 app.listen(port)
 console.log('listening port is: '+ port)
---
+++ rootRouter.js
@@ -0,0 +1,58 @@
+'use strict'
+
+var sha1 = require('sha1')
+var utils = require('./utils')
+var WxCrypto = require('./wxCrypto')
+
+module.exports = function(config){
+  return function* (next){
+    console.log(this.query)
+    var token = config.weixinOpenGongzhonghao.token
+    var signature = this.query.signature
+    var nonce = this.query.nonce
+    var timestamp = this.query.timestamp
+    var echostr = this.query.echostr
+
+    var str = [token, timestamp, nonce].sort().join('')
+    var sha1Str = sha1(str)
+
+    if(sha1Str === signature){
+      if(this.method === 'GET'){
+        console.log('request from weixin server↓↓↓↓\n' +
+            ' method is %s \n path is %s \n query is %s \n' +
+            'request from weixin server↑↑↑↑',
+            this.method, this.url, JSON.stringify(this.query))
+      }else if(this.method === 'POST'){
+        var data = yield getRawBody(this.req, { length: this.length, limit: '1mb', encoding: this.charset })
+
+        console.log('request from weixin server↓↓↓↓\n' +
+            ' method is %s \n path is %s \n data is %s \n' +
+            'request from weixin server↑↑↑↑',
+            this.method, this.url, data.toString())
+
+        var content = yield utils.parseXMLAsync(data)
+        console.log('raw data to json object\n', content)
+        var message = utils.formatMessage(content.xml)
+        console.log('json object to plain json object\n', message)
+
+        var xmlVerifyTicket = new WxCrypto(config.weixinOpenGongzhonghao.token, config.weixinOpenGongzhonghao.appID, config.weixinOpenGongzhonghao.key).decrypt(message.Encrypt)
+        var rawVerifyTicket = yield utils.parseXMLAsync(xmlVerifyTicket)
+        var verifyTicket = utils.formatMessage(rawVerifyTicket.xml)
+        console.log('componentVerifyTicket is: ', JSON.stringify(verifyTicket))
+
+        // save verifyTicket
+        utils.writeFileAsync(verify_ticket_file, verifyTicket.ComponentVerifyTicket)
+
+      }
+      this.body = 'What is happen?????'
+    }else{
+      console.log('request from other↓↓↓↓\n' +
+          ' method is %s \n path is %s \n query is %s \n' +
+          'request from other↑↑↑↑',
+          this.method, this.url, JSON.stringify(this.query))
+
+      this.body = '微信server以外的请求'
+    }
+
+  }
+}
