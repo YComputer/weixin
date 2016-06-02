526ec338a642f31f7d0e7195b580d65ed3a177e4
YComputer <thea@163.com>
2016-06-02 11:49:56 +0800

    6-2

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var port = 80
 
 var app = new koa()
 //app.use(route.get('/authGongZhongHao', gongZhongHaoMiddleware(config.weixinGongzhonghao, gongZhongHaoHandler.reply)))
-app.use(route.get('/authWeixinOpen', authWeixinOpen(config)))
+app.use(route.get('/auth/authWeixinOpen', authWeixinOpen(config)))
 app.use(route.get('/callbackOfAuthWeixinOpen', callbackOfAuthWeixinOpen(config)))
 app.use(gongZhongHaoMiddleware(config.weixinGongzhonghao, gongZhongHaoHandler.reply))
 app.use(rootRouter(config.weixinOpenGongzhonghao))
